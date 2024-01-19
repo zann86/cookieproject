f8c0f40968af00a09a5defc6102655bc46101908
suzanna <zannacullen@gmail.com>
2024-01-19 14:11:44 +0000

    intervals

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const button = document.getElementById("button");
 let cookies = 0; // start cookies count at zero
 setInterval(function () {
-cookies = cookies + 1;
-console.log("cookies", cookies);},
-1000;// 1000 milliseconds == 1 second
+  cookies = cookies + 1;
+  console.log("cookies", cookies);
+}, 1000); // 1000 milliseconds == 1 second
+const counter = document.getElementById("counter");
